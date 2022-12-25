1a0f4e0b891403bdab271556edf51757147841b4
Tom Pandolfi <89406137+tomjpandolfi@users.noreply.github.com>
2022-12-25 17:29:19 +0100

    init

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Honey Developer Documentation</span>,
+  project: {
+    link: 'https://github.com/honey-protocol',
+  },
+  chat: {
+    link: 'https://discord.com/honeydefi',
+  },
+  docsRepositoryBase: 'https://github.com/honey-protocol/dev-docs',
+  footer: {
+    text: 'Honey Developer Documentation',
+  },
+}
+
+export default config
